81e5879c1e6ff16fa87ab005f4b2feb4e1bff06d
Cristian64037 <clara03@aurora.edu>
2023-02-17 10:21:25 -0600

    Update Boards in React

--- project_at/src/Boards.js
+++ project_at/src/Boards.js
@@ -1,18 +1,22 @@
 
 import JobList from "./JobList";
 import useFetch from "./useFetch";
+import Boardss from "./Jobs.js"
 
 const Boards=()=>{
+
 
     const {data:jobs,isPending,error}= useFetch( "http://localhost:8000/Jobs");
 
     return(
-        <div className="home">
 
+        < div className="home">
 
             {error &&<div> {error}</div>}
             { isPending && <div> Loading...</div>}
-            {jobs && <JobList jobs={jobs} title={"All Jobs"} />}
+           {/* {jobs && <JobList jobs={jobs} title={"All Jobs"} />}*/}
+           <Boardss/>
+
         </div>
 
     );
