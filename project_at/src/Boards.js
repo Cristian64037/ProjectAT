43c498a0867c867ac71ea646922daa1727ee8a06
Cristian64037 <clara03@aurora.edu>
2023-02-18 09:58:03 -0600

    GotBoardsWorking

--- project_at/src/Boards.js
+++ project_at/src/Boards.js
@@ -2,19 +2,15 @@
 import JobList from "./JobList";
 import useFetch from "./useFetch";
 import Boardss from "./Jobs.js"
+import "./styles.css";
 
 const Boards=()=>{
-
 
     const {data:jobs,isPending,error}= useFetch( "http://localhost:8000/Jobs");
 
     return(
 
         < div className="home">
-
-            {error &&<div> {error}</div>}
-            { isPending && <div> Loading...</div>}
-           {/* {jobs && <JobList jobs={jobs} title={"All Jobs"} />}*/}
            <Boardss/>
 
         </div>
