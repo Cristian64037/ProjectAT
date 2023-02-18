663b9ff4b6de60f5f721c4461a0abc841b22b80d
NBoes <nicholas.2000@att.net>
2023-02-18 09:59:48 -0600

    Documents working

--- project_at/src/Documents.js
+++ project_at/src/Documents.js
@@ -1,193 +1,36 @@
+import useFetch from "./useFetch";
+
 const Documents = () => {
+    const {data:boxes , isPending , error}= useFetch( "http://localhost:8000/BoxCard");
+    console.log(boxes);
+
     return (
         <div>
             <form action="save_file.php" encType="multipart/form-data" method="post">
-                <div className="container">
-                    <p><b>Resume</b></p>
-                    <div className="scroll-container">
-                        <div className="card-wrapper">
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>December Resume</p>
-                            </div>
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>January Resume</p>
-                            </div>
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>February Resume</p>
-                            </div>
-                        </div>
-                    </div>
-                    <br/><br/>
-                    <input type="file" name="fileUpload" style={{marginRight: '10px'}}/>
-                    <input type="submit" defaultValue="Upload"/>
-                </div>
-                <style
-                    dangerouslySetInnerHTML={{__html: "\n            .scroll-container {\n                height: 150px;\n                overflow-x: scroll;\n                display: flex;\n            }\n\n            .card-wrapper {\n                display: flex;\n            }\n\n            .card {\n                width: 50%;\n                margin: 10px;\n                padding: 20px;\n                background-color: #ddd;\n                border-radius: 10px;\n                display: inline-block;\n                border: 2px solid black;\n            }\n        "}}/>
-                <div className="container">
-                    <p><b>Cover Letter</b></p>
-                    <div className="scroll-container">
-                        <div className="card-wrapper">
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>December Resume</p>
-                            </div>
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>January Resume</p>
-                            </div>
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>February Resume</p>
+                { isPending && <div>Loading...</div> }
+                { error && <div>{ error }</div> }
+                {boxes && boxes.map((box) => (
+                    <div className="doc-container" key={box.BoxCardId}>
+                        <p><b>{box.BoxCardName}</b></p>
+                        <div className="scroll-container">
+                            <div className="card-wrapper">
+                                <div className="card">
+                                    <p style={{fontSize: '12px'}}>December Resume</p>
+                                </div>
+                                <div className="card">
+                                    <p style={{fontSize: '12px'}}>January Resume</p>
+                                </div>
+                                <div className="card">
+                                    <p style={{fontSize: '12px'}}>February Resume</p>
+                                </div>
                             </div>
                         </div>
+                        <br/><br/>
+                        <form className="upload" action="save_file.php" encType="multipart/form-data" method="post">
+                            <input type="file"/>
+                        </form>
                     </div>
-                    <br/><br/>
-                    <form action="save_file.php" encType="multipart/form-data" method="post"
-                          style={{display: 'flex', alignItems: 'center'}}>
-                        <input type="file" name="fileUpload" style={{marginRight: '10px'}}/>
-                        <input type="submit" defaultValue="Upload"/>
-                    </form>
-                </div>
-                <style
-                    dangerouslySetInnerHTML={{__html: "\n            .scroll-container {\n                height: 150px;\n                overflow-x: scroll;\n                display: flex;\n            }\n\n            .card-wrapper {\n                display: flex;\n            }\n\n            .card {\n                width: 50%;\n                margin: 10px;\n                padding: 20px;\n                background-color: #ddd;\n                border-radius: 10px;\n                display: inline-block;\n                border: 2px solid black;\n            }\n        "}}/>
-                <div className="container">
-                    <p><b>Recommendations</b></p>
-                    <div className="scroll-container">
-                        <div className="card-wrapper">
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>December Resume</p>
-                            </div>
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>January Resume</p>
-                            </div>
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>February Resume</p>
-                            </div>
-                        </div>
-                    </div>
-                    <br/><br/>
-                    <form action="save_file.php" encType="multipart/form-data" method="post"
-                          style={{display: 'flex', alignItems: 'center'}}>
-                        <input type="file" name="fileUpload" style={{marginRight: '10px'}}/>
-                        <input type="submit" defaultValue="Upload"/>
-                    </form>
-                </div>
-                <style
-                    dangerouslySetInnerHTML={{__html: "\n            .scroll-container {\n                height: 150px;\n                overflow-x: scroll;\n                display: flex;\n            }\n\n            .card-wrapper {\n                display: flex;\n            }\n\n            .card {\n                width: 50%;\n                margin: 10px;\n                padding: 20px;\n                background-color: #ddd;\n                border-radius: 10px;\n                display: inline-block;\n                border: 2px solid black;\n            }\n        "}}/>
-                <div className="container">
-                    <p><b>Behavioral Questions</b></p>
-                    <div className="scroll-container">
-                        <div className="card-wrapper">
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>December Resume</p>
-                            </div>
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>January Resume</p>
-                            </div>
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>February Resume</p>
-                            </div>
-                        </div>
-                    </div>
-                    <br/><br/>
-                    <form action="save_file.php" encType="multipart/form-data" method="post"
-                          style={{display: 'flex', alignItems: 'center'}}>
-                        <input type="file" name="fileUpload" style={{marginRight: '10px'}}/>
-                        <input type="submit" defaultValue="Upload"/>
-                    </form>
-                </div>
-                <style
-                    dangerouslySetInnerHTML={{__html: "\n            .scroll-container {\n                height: 150px;\n                overflow-x: scroll;\n                display: flex;\n            }\n\n            .card-wrapper {\n                display: flex;\n            }\n\n            .card {\n                width: 50%;\n                margin: 10px;\n                padding: 20px;\n                background-color: #ddd;\n                border-radius: 10px;\n                display: inline-block;\n                border: 2px solid black;\n            }\n        "}}/>
-                <div className="container">
-                    <p><b>Technical Questions</b></p>
-                    <div className="scroll-container">
-                        <div className="card-wrapper">
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>December Resume</p>
-                            </div>
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>January Resume</p>
-                            </div>
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>February Resume</p>
-                            </div>
-                        </div>
-                    </div>
-                    <br/><br/>
-                    <form action="save_file.php" encType="multipart/form-data" method="post"
-                          style={{display: 'flex', alignItems: 'center'}}>
-                        <input type="file" name="fileUpload" style={{marginRight: '10px'}}/>
-                        <input type="submit" defaultValue="Upload"/>
-                    </form>
-                </div>
-                <style
-                    dangerouslySetInnerHTML={{__html: "\n            .scroll-container {\n                height: 150px;\n                overflow-x: scroll;\n                display: flex;\n            }\n\n            .card-wrapper {\n                display: flex;\n            }\n\n            .card {\n                width: 50%;\n                margin: 10px;\n                padding: 20px;\n                background-color: #ddd;\n                border-radius: 10px;\n                display: inline-block;\n                border: 2px solid black;\n            }\n        "}}/>
-                <div className="container">
-                    <p><b>Certifications</b></p>
-                    <div className="scroll-container">
-                        <div className="card-wrapper">
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>December Resume</p>
-                            </div>
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>January Resume</p>
-                            </div>
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>February Resume</p>
-                            </div>
-                        </div>
-                    </div>
-                    <br/><br/>
-                    <form action="save_file.php" encType="multipart/form-data" method="post"
-                          style={{display: 'flex', alignItems: 'center'}}>
-                        <input type="file" name="fileUpload" style={{marginRight: '10px'}}/>
-                        <input type="submit" defaultValue="Upload"/>
-                    </form>
-                </div>
-                <style
-                    dangerouslySetInnerHTML={{__html: "\n            .scroll-container {\n                height: 150px;\n                overflow-x: scroll;\n                display: flex;\n            }\n\n            .card-wrapper {\n                display: flex;\n            }\n\n            .card {\n                width: 50%;\n                margin: 10px;\n                padding: 20px;\n                background-color: #ddd;\n                border-radius: 10px;\n                display: inline-block;\n                border: 2px solid black;\n            }\n        "}}/>
-                <div className="container">
-                    <p><b>Awards</b></p>
-                    <div className="scroll-container">
-                        <div className="card-wrapper">
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>December Resume</p>
-                            </div>
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>January Resume</p>
-                            </div>
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>February Resume</p>
-                            </div>
-                        </div>
-                    </div>
-                    <br/><br/>
-                    <form action="save_file.php" encType="multipart/form-data" method="post"
-                          style={{display: 'flex', alignItems: 'center'}}>
-                        <input type="file" name="fileUpload" style={{marginRight: '10px'}}/>
-                        <input type="submit" defaultValue="Upload"/>
-                    </form>
-                </div>
-                <style
-                    dangerouslySetInnerHTML={{__html: "\n            .scroll-container {\n                height: 150px;\n                overflow-x: scroll;\n                display: flex;\n            }\n\n            .card-wrapper {\n                display: flex;\n            }\n\n            .card {\n                width: 50%;\n                margin: 10px;\n                padding: 20px;\n                background-color: #ddd;\n                border-radius: 10px;\n                display: inline-block;\n                border: 2px solid black;\n            }\n        "}}/>
-                <div className="container">
-                    <p><b>Other Documents</b></p>
-                    <div className="scroll-container">
-                        <div className="card-wrapper">
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>December Resume</p>
-                            </div>
-                            <div className="card">
-                                <p style={{fontSize: '12px'}}>January Resume</p>
-                            </div>
-                        </div>
-                    </div>
-                    <br/><br/>
-                    <form action="save_file.php" encType="multipart/form-data" method="post"
-                          style={{display: 'flex', alignItems: 'center'}}>
-                        <input type="file" name="fileUpload" style={{marginRight: '10px'}}/>
-                        <input type="submit" defaultValue="Upload"/>
-                    </form>
-                </div>
-                <style
-                    dangerouslySetInnerHTML={{__html: "\n            .scroll-container {\n                height: 150px;\n                overflow-x: scroll;\n                display: flex;\n            }\n\n            .card-wrapper {\n                display: flex;\n            }\n\n            .card {\n                width: 50%;\n                margin: 10px;\n                padding: 20px;\n                background-color: #ddd;\n                border-radius: 10px;\n                display: inline-block;\n                border: 2px solid black;\n            }\n        "}}/>
+                ))}
             </form>
         </div>
     );
