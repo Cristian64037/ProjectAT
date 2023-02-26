01b09d99f84698f99f54226586dde23aa5cdd3cf
NBoes <nicholas.2000@att.net>
2023-02-25 18:22:29 -0600

    Database Changes

---
+++ server/database/config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+require('dotenv').config({
+   path: path.resolve(__dirname, './.env')
+});
+
+const dbConfig = {
+    host: process.env.host,
+    port: process.env.port,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+};
+module.exports = dbConfig;
